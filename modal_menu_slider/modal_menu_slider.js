948c7f6fbe67ae207a8153756f81e8e30741cf51
sophieshcher <sophiyashcherbyna007@gmail.com>
2021-10-25 16:37:35 +0300

    added modal window

--- modal_menu_slider/modal_menu_slider.js
+++ modal_menu_slider/modal_menu_slider.js
@@ -1,4 +1,18 @@
 function onClickSlide() {
-    document.body.classList.toggle('show-nav')
+    document.body.classList.toggle('show-nav');
 
 }
+
+function onClickFixed() {
+    document.getElementById("fixed_block").style.display = "initial"
+    document.getElementById("fade").style.display = "block"
+}
+
+function onClickClose1() {
+    document.getElementById("fixed_block").style.display = "none"
+}
+
+// function onClickClose2() {
+//     document.getElementById("fixed_block").style.display = "none"
+
+// }
