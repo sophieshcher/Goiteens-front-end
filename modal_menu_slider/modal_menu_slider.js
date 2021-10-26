cbb989e5b6f4244b5efe3d283375c274475ccc97
sophieshcher <sophiyashcherbyna007@gmail.com>
2021-10-26 10:52:16 +0300

    added animation

--- modal_menu_slider/modal_menu_slider.js
+++ modal_menu_slider/modal_menu_slider.js
@@ -2,6 +2,14 @@ function onClickSlide() {
     document.body.classList.toggle('show-nav');
 
 }
+// document.getElementById("my_button").addEventListener("click", onAllClick);
+// document.getElementById("toggle").addEventListener("click", onClickSlide);
+
+// document.getElementById("open").addEventListener("click", showForm);
+
+// function showForm() {
+//     document.getElementById("modal").classList.add('show-modal');
+// }
 
 function onClickFixed() {
     document.getElementById("fixed_block").style.display = "initial"
@@ -12,7 +20,7 @@ function onClickClose1() {
     document.getElementById("fixed_block").style.display = "none"
 }
 
-// function onClickClose2() {
-//     document.getElementById("fixed_block").style.display = "none"
+function onClickClose2() {
+    document.getElementById("fixed_block").style.display = "none"
 
-// }
+}
