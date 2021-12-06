b6ba89f59297a9870eb561c98163ff27be9cd6be
sophieshcher <sophiyashcherbyna007@gmail.com>
2021-12-06 18:10:15 +0200

    addded tic-tac-toe

---
+++ Cinema/script.js
@@ -0,0 +1,669 @@
+let payValue = 0
+
+let number = 0;
+let number1 = 0;
+let number2 = 0;
+let number3 = 0;
+let number4 = 0;
+let number5 = 0;
+let number6 = 0;
+let number7 = 0;
+let number8 = 0;
+let number9 = 0;
+let number10 = 0;
+let number11 = 0;
+let number12 = 0;
+let number13 = 0;
+let number14 = 0;
+let number15 = 0;
+let number16 = 0;
+let number17 = 0;
+let number18 = 0;
+let number19 = 0;
+let number20 = 0;
+let number21 = 0;
+let number22 = 0;
+let number23 = 0;
+let number24 = 0;
+let number25 = 0;
+let number26 = 0;
+let number27 = 0;
+let number28 = 0;
+let number29 = 0;
+let number30 = 0;
+let number31 = 0;
+let number32 = 0;
+let number33 = 0;
+let number34 = 0;
+let number35 = 0;
+let number36 = 0;
+
+let idArray = []
+let idArrayNew = []
+
+
+
+document.getElementById("button1").addEventListener("click", button1CklicOn)
+function button1CklicOn(e) {
+
+    if (number === 0) {
+    document.getElementById("button1").style.background = "rgb(179, 179, 179)";
+    // idArray.push(e.target.id)
+    payValue += 70;
+    number++;
+    console.log(number)
+   }else {
+    document.getElementById("button1").style.background = "rgb(144, 238, 144)";
+    payValue -= 70;
+    // idArray.pop(e.target.id)
+    number--;
+
+
+    
+}
+
+}
+
+document.getElementById("button2").addEventListener("click", button2CklicOn)
+function button2CklicOn() {
+
+    if (number1 === 0) {
+    document.getElementById("button2").style.background = "rgb(179, 179, 179)";
+    payValue += 70;
+    ++number1;
+    console.log(number)
+   }else{
+    document.getElementById("button2").style.background = "rgb(144, 238, 144)";
+    payValue -= 70;
+    --number1;
+}
+
+}
+
+document.getElementById("button3").addEventListener("click", button3CklicOn)
+function button3CklicOn() {
+
+    if (number2 === 0) {
+    document.getElementById("button3").style.background = "rgb(179, 179, 179)";
+    payValue += 70;
+    ++number2;
+    console.log(number)
+   }else{
+    document.getElementById("button3").style.background = "rgb(144, 238, 144)";
+    payValue -= 70;
+    --number2;
+}
+    
+}
+
+document.getElementById("button4").addEventListener("click", button4CklicOn)
+function button4CklicOn() {
+    //  number = 0;
+    if (number3 === 0) {
+    document.getElementById("button4").style.background = "rgb(179, 179, 179)";
+    payValue += 70;
+    ++number3;
+    console.log(number)
+   }else{
+    document.getElementById("button4").style.background = "rgb(144, 238, 144)";
+    payValue -= 70;
+    --number3;
+}
+    
+}
+
+document.getElementById("button5").addEventListener("click", button5CklicOn)
+function button5CklicOn() {
+    //  number = 0;
+    if (number4 === 0) {
+    document.getElementById("button5").style.background = "rgb(179, 179, 179)";
+    payValue += 70;
+    ++number4;
+    console.log(number)
+   }else{
+    document.getElementById("button5").style.background = "rgb(144, 238, 144)";
+    payValue -= 70;
+    --number4;
+}
+    
+}
+
+document.getElementById("button6").addEventListener("click", button6CklicOn)
+function button6CklicOn() {
+    //  number = 0;
+    if (number5 === 0) {
+    document.getElementById("button6").style.background = "rgb(179, 179, 179)";
+    payValue += 70;
+    ++number5;
+    console.log(number)
+   }else{
+    document.getElementById("button6").style.background = "rgb(144, 238, 144)";
+    payValue -= 70;
+    --number5;
+}
+    
+}
+
+document.getElementById("button7").addEventListener("click", button7CklicOn)
+function button7CklicOn() {
+    //  number = 0;
+    if (number6 === 0) {
+    document.getElementById("button7").style.background = "rgb(179, 179, 179)";
+    payValue += 70;
+    ++number6;
+    console.log(number)
+   }else{
+    document.getElementById("button7").style.background = "rgb(144, 238, 144)";
+    payValue -= 70;
+    --number6;
+}
+    
+}
+
+document.getElementById("button8").addEventListener("click", button8CklicOn)
+function button8CklicOn() {
+    //  number = 0;
+    if (number7 === 0) {
+    document.getElementById("button8").style.background = "rgb(179, 179, 179)";
+    payValue += 70;
+    ++number7;
+    console.log(number)
+   }else{
+    document.getElementById("button8").style.background = "rgb(144, 238, 144)";
+    payValue -= 70;
+    --number7;
+}
+    
+}
+
+document.getElementById("button9").addEventListener("click", button9CklicOn)
+function button9CklicOn() {
+    //  number = 0;
+    if (number8 === 0) {
+    document.getElementById("button9").style.background = "rgb(179, 179, 179)";
+    payValue += 70;
+    ++number8;
+    console.log(number)
+   }else{
+    document.getElementById("button9").style.background = "rgb(144, 238, 144)";
+    payValue -= 70;
+    --number8;
+}
+    
+}
+
+document.getElementById("button10").addEventListener("click", button10CklicOn)
+function button10CklicOn() {
+    // number = 0;
+    if (number9 === 0) {
+    document.getElementById("button10").style.background = "rgb(179, 179, 179)";
+    payValue += 70;
+    ++number9;
+    console.log(number)
+   }else{
+    document.getElementById("button10").style.background = "rgb(144, 238, 144)";
+    payValue -= 70;
+    --number9;
+}
+    
+}
+
+document.getElementById("button11").addEventListener("click", button11CklicOn)
+function button11CklicOn() {
+    // number = 0;
+    if (number10 === 0) {
+    document.getElementById("button11").style.background = "rgb(179, 179, 179)";
+    payValue += 70;
+    ++number10;
+    console.log(number)
+   }else{
+    document.getElementById("button11").style.background = "rgb(144, 238, 144)";
+    payValue -= 70;
+    --number10;
+}
+    
+}
+
+document.getElementById("button12").addEventListener("click", button12CklicOn)
+function button12CklicOn() {
+    // number = 0;
+    if (number11 === 0) {
+    document.getElementById("button12").style.background = "rgb(179, 179, 179)";
+    payValue += 70;
+    ++number11;
+    console.log(number)
+   }else{
+    document.getElementById("button12").style.background = "rgb(144, 238, 144)";
+    payValue -= 70;
+    --number11;
+}
+    
+}
+
+document.getElementById("button13").addEventListener("click", button13CklicOn)
+function button13CklicOn() {
+    // number = 0;
+    if (number12 === 0) {
+    document.getElementById("button13").style.background = "rgb(179, 179, 179)";
+    payValue += 70;
+    ++number12;
+    console.log(number)
+   }else{
+    document.getElementById("button13").style.background = "rgb(144, 238, 144)";
+    payValue -= 70;
+    --number12;
+}
+    
+}
+
+document.getElementById("button14").addEventListener("click", button14CklicOn)
+function button14CklicOn() {
+    // number = 0;
+    if (number13 === 0) {
+    document.getElementById("button14").style.background = "rgb(179, 179, 179)";
+    payValue += 70;
+    ++number13;
+    console.log(number)
+   }else{
+    document.getElementById("button14").style.background = "rgb(144, 238, 144)";
+    payValue -= 70;
+    --number13;
+}
+    
+}
+
+document.getElementById("button15").addEventListener("click", button15CklicOn)
+function button15CklicOn() {
+    // number = 0;
+    if (number14 === 0) {
+    document.getElementById("button15").style.background = "rgb(179, 179, 179)";
+    payValue += 70;
+    ++number14;
+    console.log(number)
+   }else{
+    document.getElementById("button15").style.background = "rgb(144, 238, 144)";
+    payValue -= 70;
+    --number14;
+}
+    
+}
+
+document.getElementById("button16").addEventListener("click", button16CklicOn)
+function button16CklicOn() {
+    // number = 0;
+    if (number15 === 0) {
+    document.getElementById("button16").style.background = "rgb(179, 179, 179)";
+    payValue += 70;
+    ++number15;
+    console.log(number)
+   }else{
+    document.getElementById("button16").style.background = "rgb(144, 238, 144)";
+    payValue -= 70;
+    --number15;
+}
+    
+}
+
+document.getElementById("button17").addEventListener("click", button17CklicOn)
+function button17CklicOn() {
+    // number = 0;
+    if (number16 === 0) {
+    document.getElementById("button17").style.background = "rgb(179, 179, 179)";
+    payValue += 70;
+    ++number16;
+    console.log(number)
+   }else{
+    document.getElementById("button17").style.background = "rgb(144, 238, 144)";
+    payValue -= 70;
+    --number16;
+}
+    
+}
+
+document.getElementById("button18").addEventListener("click", button18CklicOn)
+function button18CklicOn() {
+    // number = 0;
+    if (number17 === 0) {
+    document.getElementById("button18").style.background = "rgb(179, 179, 179)";
+    payValue += 70;
+    ++number17;
+    console.log(number)
+   }else{
+    document.getElementById("button18").style.background = "rgb(144, 238, 144)";
+    payValue -= 70;
+    --number17;
+}
+    
+}
+
+document.getElementById("button19").addEventListener("click", button19CklicOn)
+function button19CklicOn() {
+    // number = 0;
+    if (number18 === 0) {
+    document.getElementById("button19").style.background = "rgb(179, 179, 179)";
+    payValue += 70;
+    ++number18;
+    console.log(number)
+   }else{
+    document.getElementById("button19").style.background = "rgb(144, 238, 144)";
+    payValue -= 70;
+    --number18;
+}
+    
+}
+
+document.getElementById("button20").addEventListener("click", button20CklicOn)
+function button20CklicOn() {
+    // number = 0;
+    if (number19 === 0) {
+    document.getElementById("button20").style.background = "rgb(179, 179, 179)";
+    payValue += 70;
+    ++number19;
+    console.log(number)
+   }else{
+    document.getElementById("button20").style.background = "rgb(144, 238, 144)";
+    payValue -= 70;
+    --number19;
+}
+    
+}
+
+document.getElementById("button21").addEventListener("click", button21CklicOn)
+function button21CklicOn() {
+    // number = 0;
+    if (number20 === 0) {
+    document.getElementById("button21").style.background = "rgb(179, 179, 179)";
+    payValue += 70;
+    ++number20;
+    console.log(number)
+   }else{
+    document.getElementById("button21").style.background = "rgb(144, 238, 144)";
+    payValue -= 70;
+    --number20;
+}
+    
+}
+
+document.getElementById("button22").addEventListener("click", button22CklicOn)
+function button22CklicOn() {
+    // number = 0;
+    if (number21 === 0) {
+    document.getElementById("button22").style.background = "rgb(179, 179, 179)";
+    payValue += 70;
+    ++number21;
+    console.log(number)
+   }else{
+    document.getElementById("button22").style.background = "rgb(144, 238, 144)";
+    payValue -= 70;
+    --number21;
+}
+    
+}
+
+document.getElementById("button23").addEventListener("click", button23CklicOn)
+function button23CklicOn() {
+    // number = 0;
+    if (number22 === 0) {
+    document.getElementById("button23").style.background = "rgb(179, 179, 179)";
+    payValue += 70;
+    ++number22;
+    console.log(number)
+   }else{
+    document.getElementById("button23").style.background = "rgb(144, 238, 144)";
+    payValue -= 70;
+    --number22;
+}
+    
+}
+
+document.getElementById("button24").addEventListener("click", button24CklicOn)
+function button24CklicOn() {
+    // number = 0;
+    if (number23 === 0) {
+    document.getElementById("button24").style.background = "rgb(179, 179, 179)";
+    payValue += 70;
+    ++number23;
+    console.log(number)
+   }else{
+    document.getElementById("button24").style.background = "rgb(144, 238, 144)";
+    payValue -= 70;
+    --number23;
+}
+    
+}
+
+document.getElementById("button25").addEventListener("click", button25CklicOn)
+function button25CklicOn() {
+    // number = 0;
+    if (number24 === 0) {
+    document.getElementById("button25").style.background = "rgb(179, 179, 179)";
+    payValue += 70;
+    ++number24;
+    console.log(number)
+   }else{
+    document.getElementById("button25").style.background = "rgb(144, 238, 144)";
+    payValue -= 70;
+    --number24;
+}
+    
+}
+
+document.getElementById("button26").addEventListener("click", button26CklicOn)
+function button26CklicOn() {
+    // number = 0;
+    if (number25 === 0) {
+    document.getElementById("button26").style.background = "rgb(179, 179, 179)";
+    payValue += 70;
+    ++number25;
+    console.log(number)
+   }else{
+    document.getElementById("button26").style.background = "rgb(144, 238, 144)";
+    payValue -= 70;
+    --number25;
+}
+    
+}
+
+document.getElementById("button27").addEventListener("click", button27CklicOn)
+function button27CklicOn() {
+    // number = 0;
+    if (number26 === 0) {
+    document.getElementById("button27").style.background = "rgb(179, 179, 179)";
+    payValue += 70;
+    ++number26;
+    console.log(number)
+   }else{
+    document.getElementById("button27").style.background = "rgb(144, 238, 144)";
+    payValue -= 70;
+    --number26;
+}
+    
+}
+
+document.getElementById("button28").addEventListener("click", button28CklicOn)
+function button28CklicOn() {
+    // number = 0;
+    if (number27 === 0) {
+    document.getElementById("button28").style.background = "rgb(179, 179, 179)";
+    payValue += 100;
+    ++number27;
+    console.log(number)
+   }else{
+    document.getElementById("button28").style.background = "rgb(210, 182, 255)";
+    payValue -= 100;
+    --number27;
+}
+    
+}
+
+document.getElementById("button29").addEventListener("click", button29CklicOn)
+function button29CklicOn() {
+    // number = 0;
+    if (number28 === 0) {
+    document.getElementById("button29").style.background = "rgb(179, 179, 179)";
+    payValue += 100;
+    ++number28;
+    console.log(number)
+   }else{
+    document.getElementById("button29").style.background = "rgb(210, 182, 255)";
+    payValue -= 100;
+    --number28;
+}
+    
+}
+
+document.getElementById("button30").addEventListener("click", button30CklicOn)
+function button30CklicOn() {
+    // number = 0;
+    if (number29 === 0) {
+    document.getElementById("button30").style.background = "rgb(179, 179, 179)";
+    payValue += 100;
+    ++number29;
+    console.log(number)
+   }else{
+    document.getElementById("button30").style.background = "rgb(210, 182, 255)";
+    payValue -= 100;
+    --number29;
+}
+    
+}
+
+document.getElementById("button31").addEventListener("click", button31CklicOn)
+function button31CklicOn() {
+    // number = 0;
+    if (number30 === 0) {
+    document.getElementById("button31").style.background = "rgb(179, 179, 179)";
+    payValue += 100;
+    ++number30;
+    console.log(number)
+   }else{
+    document.getElementById("button31").style.background = "rgb(210, 182, 255)";
+    payValue -= 100;
+    --number30;
+}
+    
+}
+
+document.getElementById("button32").addEventListener("click", button32CklicOn)
+function button32CklicOn() {
+    // number = 0;
+    if (number31 === 0) {
+    document.getElementById("button32").style.background = "rgb(179, 179, 179)";
+    payValue += 100;
+    ++number31;
+    console.log(number)
+   }else{
+    document.getElementById("button32").style.background = "rgb(210, 182, 255)";
+    payValue -= 100;
+    --number31;
+}
+    
+}
+
+document.getElementById("button33").addEventListener("click", button33CklicOn)
+function button33CklicOn() {
+    // number = 0;
+    if (number32 === 0) {
+    document.getElementById("button33").style.background = "rgb(179, 179, 179)";
+    payValue += 100;
+    ++number32;
+    console.log(number)
+   }else{
+    document.getElementById("button33").style.background = "rgb(210, 182, 255)";
+    payValue -= 100;
+    --number32;
+}
+    
+}
+
+document.getElementById("button34").addEventListener("click", button34CklicOn)
+function button34CklicOn() {
+    // number = 0;
+    if (number33 === 0) {
+    document.getElementById("button34").style.background = "rgb(179, 179, 179)";
+    payValue += 100;
+    ++number33;
+    console.log(number)
+   }else{
+    document.getElementById("button34").style.background = "rgb(210, 182, 255)";
+    payValue -= 100;
+    --number33;
+}
+    
+}
+
+document.getElementById("button35").addEventListener("click", button35CklicOn)
+function button35CklicOn() {
+    // number = 0;
+    if (number34 === 0) {
+    document.getElementById("button35").style.background = "rgb(179, 179, 179)";
+    payValue += 100;
+    ++number34;
+    console.log(number)
+   }else{
+    document.getElementById("button35").style.background = "rgb(210, 182, 255)";
+    payValue -= 100;
+    --number34;
+}
+    
+}
+
+document.getElementById("button36").addEventListener("click", button36CklicOn)
+function button36CklicOn() {
+    // number = 0;
+    if (number35 === 0) {
+    document.getElementById("button36").style.background = "rgb(179, 179, 179)";
+    payValue += 100;
+    ++number35;
+    console.log(number)
+   }else{
+    document.getElementById("button36").style.background = "rgb(210, 182, 255)";
+    payValue -= 100;
+    --number35;
+}
+    
+}
+
+document.getElementById("button37").addEventListener("click", button37CklicOn)
+function button37CklicOn() {
+    // number = 0;
+    if (number36 === 0) {
+    document.getElementById("button37").style.background = "rgb(179, 179, 179)";
+    payValue += 100;
+    ++number36;
+    console.log(number)
+   }else{
+    document.getElementById("button37").style.background = "rgb(210, 182, 255)";
+    payValue -= 100;
+    --number36;
+}
+    
+}
+
+document.getElementById("submitButton").addEventListener("click", submitButtonFunction)
+
+function submitButtonFunction() {
+    document.getElementById("HowMuchToPay").style.display = "block"
+    document.getElementById("YourPrice").textContent = payValue + " грн"
+}
+
+document.getElementById("submitButtonTwo").addEventListener("click", submitButtonTwoFunction)
+
+function submitButtonTwoFunction() {
+    document.getElementById("HowMuchToPay").style.display = "none"
+    console.log(document.getElementById("cheaperSection").childNodes.length)
+    for( let i = 0; i < document.getElementById("moreExpensiveSection").childNodes.length; i++) {
+        idArray.push(document.getElementById("moreExpensiveSection").childNodes[i])
+        console.log(document.getElementById("moreExpensiveSection").childNodes[i])
+
+
+        if(document.getElementById("moreExpensiveSection").childNodes[i].includes = "id"){
+            idArrayNew.push(document.getElementById("moreExpensiveSection").childNodes[i]) 
+            
+        }
+
+        
+    }
+    // animation-name: modalclose;
+    // animation-duration: .6s;
+}
